3dfc37cad4463422a6f557196d5d15159f60d29c
S. Mannem <sebastiaan.mannem@splendiddata.com>
2017-01-10 12:36:42 +0000

    site.pp with less module specific stuff in it

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,34 +1,14 @@
-class postgrespure_repo {
-
-  package { 'http://base.splendiddata.com/postgrespure/4/centos/7/noarch/postgrespure-release-latest.rpm':
-    ensure => 'installed',
-  }
+node default {
+   #include pure_repo_old
+   include pure
 }
 
-class which {
-
-  package { 'which':
-    ensure => 'installed',
-  }
-
+class pure_repo_old{
+   yumrepo { "PostgresPURE":
+      baseurl => "http://base.splendiddata.com/postgrespure/4/$operatingsystemrelease/$architecture",
+      descr => "Postgres PURE",
+      enabled => 1,
+      gpgcheck => 0
+   }
 }
-
-class mlocate {
-
-  package { 'mlocate':
-    ensure => 'installed',
-  }
-
-}
-
-#repmgr module uses locate???
-class { 'which': }
-class { 'mlocate': }
-
-exec { 'updatedb':                # update locate db
-  command => '/usr/bin/updatedb'  # command this resource will run
-}
-
-#Below has issues (first time it installs, after that it wines that package is already installed. How idempotent is that???
-#class { 'postgrespure_repo': }
 
