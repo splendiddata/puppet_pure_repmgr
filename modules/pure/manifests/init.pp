4cd0056863477b56f800b941adee225d9e8ca5a3
S. Mannem <sebastiaan.mannem@splendiddata.com>
2017-01-10 12:35:47 +0000

    pure module now works

--- modules/pure/manifests/init.pp
+++ modules/pure/manifests/init.pp
@@ -3,21 +3,29 @@
 # Installs purerepo from rpm on url
 class pure
 (
-  $repo              = 'http://base.splendiddata.com/postgrespure'
-  $version           = '4'
-  $package_name      = 'postgrespure-release'
-  $package_version   = 'latest'
-)
+  $repo              = $pure::params::repo,
+  $version           = $pure::params::version,
+  $package_name      = $pure::params::package_name,
+  $package_version   = $pure::params::package_version
+) inherits pure::params
 {
   $dist              = $::operatingsystem ?
   {
-    'Centos' => [ 'centos' ]
+    'Centos' => 'centos'
   }
-  $dist_version      = $::osreleasemajor
-  $package_url       = "${repo}/${version}/${dist}/${dist_version}/noarch/${package_name}-${package_version}.rpm"
+  $dist_version      = $facts['os']['release']['major']
+# $dist_version      = $::osreleasemajor
 
-  # Ensure packages are installed / up to date.
-  class { 'pure::package_url':
-  }
-  contain 'pure::package_url'
+#  $package_url       = "${repo}/${version}/${dist}/${dist_version}/noarch/${package_name}-${package_version}.rpm"
+  $repo_url       = "${repo}/${version}/${dist}/${dist_version}/"
+#  $repo_url       = "http://base.splendiddata.com/postgrespure/4/centos/7/"
+#  $repo_url       = "http://base.splendiddata.com/postgrespure/${version}/${dist}/${dist_version}/"
+
+  yumrepo { "PostgresPURE":
+      baseurl => "${repo_url}",
+      descr => "Postgres PURE",
+      enabled => 1,
+      gpgcheck => 0
+    }
+
 }
--- modules/pure/manifests/install.pp
+++
@@ -1,8 +0,0 @@
-# == Class pure::install
-# Private class
-class pure::install
-{
-  package { $pure::common_package_name: ensure => $pure::common_package_version }
-  package { $pure::pg_package_name: ensure => $pure::pg_package_version }
-  package { $pure::additional_packages: ensure => installed }
-}
