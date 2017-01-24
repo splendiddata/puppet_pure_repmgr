7ed233240530af4f0894a15ec5dac1a4f414ea0d
S. Mannem <sebastiaan.mannem@splendiddata.com>
2017-01-24 17:40:41 +0000

    Added some stuff for puppetdb. Installed in another way, but this seems like a valid example...

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,3 +1,11 @@
+node puppet {
+   class { 'puppetdb':
+   }
+
+   class { 'puppetdb::master::config':
+   }
+}
+
 node default {
    class { 'pure_repmgr':
       primarynetwork      => '172.17.0.0/24',
