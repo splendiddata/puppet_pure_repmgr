d0ca330b9c3f15cfe0b45cf4562cd5a8c08f35da
S. Mannem <sebastiaan.mannem@splendiddata.com>
2017-02-24 02:44:27 +0000

    pure_cluster_logger.service: ensure => running

--- manifests/cluster_logger.pp
+++ manifests/cluster_logger.pp
@@ -6,28 +6,33 @@ class pure_repmgr::cluster_logger
 ) inherits pure_repmgr
 {
 
-      file { ['/usr/pgpure/cluster_logger', '/var/log/pgpure/cluster_logger' ]:
-         ensure => directory,
-         owner  => $pure_postgres::postgres_user,
-         group  => $pure_postgres::postgres_group,
-      } ->
+   file { ['/usr/pgpure/cluster_logger', '/var/log/pgpure/cluster_logger' ]:
+      ensure => directory,
+      owner  => $pure_postgres::postgres_user,
+      group  => $pure_postgres::postgres_group,
+   } ->
 
-      file {'/usr/pgpure/cluster_logger/pure_cluster_logger.py':
-         path    => '/usr/pgpure/cluster_logger/pure_cluster_logger.py',
-         ensure  => 'file',
-         source  => 'puppet:///modules/pure_repmgr/pure_cluster_logger.py',
-         owner   => $pure_postgres::postgres_user,
-         group   => $pure_postgres::postgres_group,
-         mode    => '0750',
-      } ->
+   file {'/usr/pgpure/cluster_logger/pure_cluster_logger.py':
+      path    => '/usr/pgpure/cluster_logger/pure_cluster_logger.py',
+      ensure  => 'file',
+      source  => 'puppet:///modules/pure_repmgr/pure_cluster_logger.py',
+      owner   => $pure_postgres::postgres_user,
+      group   => $pure_postgres::postgres_group,
+      mode    => '0750',
+   } ->
 
-      file {'/usr/lib/systemd/system/pure_cluster_logger.service':
-         path    => '/usr/lib/systemd/system/pure_cluster_logger.service',
-         ensure  => 'file',
-         source  => 'puppet:///modules/pure_repmgr/pure_cluster_logger.service',
-         owner   => 'root',
-         group   => 'root',
-         mode    => '0644',
-      }
+   file {'/usr/lib/systemd/system/pure_cluster_logger.service':
+      path    => '/usr/lib/systemd/system/pure_cluster_logger.service',
+      ensure  => 'file',
+      source  => 'puppet:///modules/pure_repmgr/pure_cluster_logger.service',
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+   } ->
+
+   Service { 'pure_cluster_logger.service':
+      ensure => 'running',
+      enable => true,
+   }
 
 }
