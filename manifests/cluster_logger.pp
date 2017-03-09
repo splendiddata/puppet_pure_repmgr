fbb4d6a7910fac983db1037875089e8b31e6fa38
S. Mannem <sebastiaan.mannem@splendiddata.com>
2017-03-09 16:20:56 +0000

    cluster_logger is now running as a different user then root. And I fixed another stability issue in the cluster logger script.

--- manifests/cluster_logger.pp
+++ manifests/cluster_logger.pp
@@ -6,30 +6,63 @@ class pure_repmgr::cluster_logger
 ) inherits pure_repmgr
 {
 
+  split($facts['pure_cloud_nodes'],",").each | String $source | {
+    pure_postgres::pg_hba {"pg_hba entry for pure_cluster_logger from $source":
+      database        => 'postgres',
+      method          => 'trust',
+      state           => 'present',
+      source          => "${source}/32",
+      connection_type => 'host',
+      user            => 'pure_cluster_logger',
+      notify          => Class['pure_postgres::reload'],
+    }
+  }
+
+  pure_postgres::role {'pure_cluster_logger':
+    canlogin => true,
+  } ->
+
+  user { 'pure_cluster_logger':
+    ensure     => present,
+    comment    => 'postgrespure cluster logging service',
+    groups     => 'pgpure',
+    home       => '/var/log/pgpure/cluster_logger',
+    managehome => true,
+    shell      => '/sbin/nologin',
+    system     => true,
+  } ->
+
+  file { [ '/usr/pgpure/cluster_logger', '/var/log/pgpure/cluster_logger' ]:
+    ensure => directory,
+    owner  => 'pure_cluster_logger',
+    group  => 'pgpure',
+  } ->
+
   file { "${pure_postgres::pg_etc_dir}/cluster_logger.ini":
     ensure  => file,
     content => epp('pure_repmgr/cluster_logger.epp'),
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
+    owner   => 'pure_cluster_logger',
+    group   => 'pgpure',
+    mode    => '0640',
     replace =>  false,
     notify  => Service['pure_cluster_logger.service'],
-  } ->
-
-  file { ['/usr/pgpure/cluster_logger', '/var/log/pgpure/cluster_logger' ]:
-    ensure => directory,
-    owner  => $pure_postgres::postgres_user,
-    group  => $pure_postgres::postgres_group,
   } ->
 
   file {'/usr/pgpure/cluster_logger/pure_cluster_logger.py':
     path    => '/usr/pgpure/cluster_logger/pure_cluster_logger.py',
     ensure  => 'file',
     content => epp('pure_repmgr/pure_cluster_logger.epp'),
-    owner   => $pure_postgres::postgres_user,
-    group   => $pure_postgres::postgres_group,
+    owner   => 'pure_cluster_logger',
+    group   => 'pgpure',
     mode    => '0750',
     notify  => Service['pure_cluster_logger.service'],
+  } ->
+
+  file {'/var/log/pgpure/cluster_logger/cluster_logger.log':
+    ensure  => 'file',
+    owner   => 'pure_cluster_logger',
+    group   => 'pgpure',
+    mode    => '0640',
   } ->
 
   file {'/usr/lib/systemd/system/pure_cluster_logger.service':
