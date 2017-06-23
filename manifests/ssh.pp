04fb59dfdb78817e21c440e482e259ba96d8be0b
S. Mannem <sebastiaan.mannem@splendiddata.com>
2017-06-23 12:25:50 +0200

    Added a copyright disclaimer to every manifest

--- manifests/ssh.pp
+++ manifests/ssh.pp
@@ -1,3 +1,20 @@
+# Copyright (C) 2017 Collaboration of KPN and Splendid Data
+#
+# This file is part of puppet_pure_postgres.
+#
+# puppet_pure_barman is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# puppet_pure_postgres is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with puppet_pure_postgres.  If not, see <http://www.gnu.org/licenses/>.
+
 # == Class: pure_repmgr::ssh
 #
 # Configure a replicated cluster with repmgr from pure repo 
